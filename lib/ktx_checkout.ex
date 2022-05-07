f787dec5618c6916583714f1149b5206c5ebf7c1
Alejandro Meneses <almenesesp@gmail.com>
2022-05-06 19:07:20 -0500

    First commit!

---
+++ lib/ktx_checkout.ex
@@ -0,0 +1,18 @@
+defmodule KtxCheckout do
+  @moduledoc """
+  Documentation for `KtxCheckout`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KtxCheckout.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ktx_checkout_test.exs
@@ -0,0 +1,8 @@
+defmodule KtxCheckoutTest do
+  use ExUnit.Case
+  doctest KtxCheckout
+
+  test "greets the world" do
+    assert KtxCheckout.hello() == :world
+  end
+end
